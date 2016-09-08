5f97c6a343c31094728b58f8ed82bea318ddd074
Qinghang Wu <Qinghang Wu>
2016-09-08 10:59:19 +1000

    A simple multi-threading winform application

---
+++ SimpleMultiThreading/MultiThreading.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SimpleMultiThreading
+{
+    partial class MultiThreading
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Red = new System.Windows.Forms.Button();
+            this.btn_Blue = new System.Windows.Forms.Button();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Red
+            // 
+            this.btn_Red.Location = new System.Drawing.Point(238, 478);
+            this.btn_Red.Name = "btn_Red";
+            this.btn_Red.Size = new System.Drawing.Size(75, 23);
+            this.btn_Red.TabIndex = 0;
+            this.btn_Red.Text = "Draw Red";
+            this.btn_Red.UseVisualStyleBackColor = true;
+            this.btn_Red.Click += new System.EventHandler(this.btn_Red_Click);
+            // 
+            // btn_Blue
+            // 
+            this.btn_Blue.Location = new System.Drawing.Point(343, 478);
+            this.btn_Blue.Name = "btn_Blue";
+            this.btn_Blue.Size = new System.Drawing.Size(75, 23);
+            this.btn_Blue.TabIndex = 1;
+            this.btn_Blue.Text = "Draw Blue";
+            this.btn_Blue.UseVisualStyleBackColor = true;
+            this.btn_Blue.Click += new System.EventHandler(this.btn_Blue_Click);
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Location = new System.Drawing.Point(449, 478);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_Refresh.TabIndex = 2;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // MultiThreading
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 523);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.btn_Blue);
+            this.Controls.Add(this.btn_Red);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MultiThreading";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Multi-Threading WinForm Application";
+            this.Load += new System.EventHandler(this.MultiThreading_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Red;
+        private System.Windows.Forms.Button btn_Blue;
+        private System.Windows.Forms.Button btn_Refresh;
+    }
+}
+
---
+++ SimpleMultiThreading/MultiThreading.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SimpleMultiThreading
+{
+    public partial class MultiThreading : Form
+    {
+        private Thread t_Red, t_Blue;
+        private Random rdm;
+
+        public MultiThreading()
+        {
+            InitializeComponent();
+        }
+
+        private void MultiThreading_Load(object sender, EventArgs e)
+        {
+            rdm = new Random();
+        }
+
+        private void btn_Red_Click(object sender, EventArgs e)
+        {
+            t_Red = new Thread(thread_Red);
+            t_Red.Start();
+        }
+
+        private void thread_Red()
+        {
+
+            for (int i = 0; i < 10; i++ )
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 5), new Rectangle(rdm.Next(0, this.Width), rdm.Next(0, this.Height), 30, 30));
+                Thread.Sleep(1000);
+            }
+            MessageBox.Show("Red Complete");
+        }
+
+        private void thread_Blue()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 5), new Rectangle(rdm.Next(0, this.Width), rdm.Next(0, this.Height), 30, 30));
+                Thread.Sleep(1000);
+            }
+            MessageBox.Show("Blue Complete");
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void btn_Blue_Click(object sender, EventArgs e)
+        {
+            t_Blue = new Thread(thread_Blue);
+            t_Blue.Start();
+        }
+    }
+}
